3b4a41b100972f35464ef3dcb300ff0ae4b4ae79
Oodapow <petre.v.alexandru@gmail.com>
2020-04-15 17:54:37 +0300

    fix helm main

--- infrastructure/helm/main.py
+++ infrastructure/helm/main.py
@@ -1,4 +1,5 @@
 from sys import platform
+import subprocess
 import os
 
 HELM_VERSION = '2.15.1'
@@ -8,10 +9,10 @@ if __name__ == '__main__':
 
     if platform == "linux" or platform == "linux2":
         print('Running Ubuntu Setup')
-        os.system('curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/helm/os/ubuntu/main.sh | sudo sh')
+        subprocess.check_call('curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/helm/os/ubuntu/main.sh | sudo sh'.split(' '))
     elif platform == "darwin":
         print('Running macOS Setup')
-        os.system('curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/helm//os/mac/main.sh | sudo sh')
+        subprocess.check_call('curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/helm//os/mac/main.sh | sudo sh'.split(' '))
     elif platform == "win32":
         print('Running Windows Setup')
-        os.system('curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/helm/os/windows/main.py | python')
+        subprocess.check_call('curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/helm/os/windows/main.py | python'.split(' '))
