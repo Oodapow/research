729a38f0cf37286383336b45acfdcc46d2c070bc
Oodapow <petre.v.alexandru@gmail.com>
2020-04-15 18:06:51 +0300

    fix

--- infrastructure/helm/main.py
+++ infrastructure/helm/main.py
@@ -1,18 +1,32 @@
 from sys import platform
-import subprocess
+from subprocess import Popen, PIPE
 import os
 
 HELM_VERSION = '2.15.1'
 
+def run_script(url, shell):
+    with Popen(['curl', '-fsSL', url], stdout=PIPE) as curl:
+        with Popen([shell], stdin=curl.stdout, stdout=PIPE) as shell:
+            curl.stdout.close()
+            curl.wait()
+            shell.wait()
+    
+
 if __name__ == '__main__':
     os.environ['HELM_VERSION'] = HELM_VERSION
 
+    url = None
+    shell = 'sh'
+
     if platform == "linux" or platform == "linux2":
         print('Running Ubuntu Setup')
-        subprocess.check_call('curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/helm/os/ubuntu/main.sh | sudo sh'.split(' '))
+        url = 'https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/helm/os/ubuntu/main.sh'
     elif platform == "darwin":
         print('Running macOS Setup')
-        subprocess.check_call('curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/helm//os/mac/main.sh | sudo sh'.split(' '))
+        url = 'https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/helm//os/mac/main.sh'
     elif platform == "win32":
         print('Running Windows Setup')
-        subprocess.check_call('curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/helm/os/windows/main.py | python'.split(' '))
+        url = 'https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/helm/os/windows/main.py'
+        shell = 'python'
+    
+    run_script(url, shell)
