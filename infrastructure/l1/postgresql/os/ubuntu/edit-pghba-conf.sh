145025a1571497422fba94c9cb58a12bc937b5fc
Oodapow <petre.v.alexandru@gmail.com>
2020-05-12 21:41:19 +0300

    fix psql

--- infrastructure/l1/postgresql/os/ubuntu/edit-pghba-conf.sh
+++ infrastructure/l1/postgresql/os/ubuntu/edit-pghba-conf.sh
@@ -1,20 +1,20 @@
 ipv4str="host    all             all              0.0.0.0/0                       md5"
 ipv6str="host    all             all              ::/0                            md5"
 
-if grep -q ipv4str $1 ; then
+if grep -q $ipv4str $1 ; then
     echo "Correct IPv4 settings found, doing nothing"
 fi
 
-if ! grep -q ipv4str $1 ; then
+if ! grep -q $ipv4str $1 ; then
     echo "No IPv4 settings found, adding one at the end"
-    echo ipv4str >> $1
+    echo $ipv4str >> $1
 fi
 
-if grep -q ipv6str $1 ; then
+if grep -q $ipv6str $1 ; then
     echo "Correct IPv6 settings found, doing nothing"
 fi
 
-if ! grep -q ipv6str $1 ; then
+if ! grep -q $ipv6str $1 ; then
     echo "No IPv6 settings found, adding one at the end"
-    echo ipv6str >> $1
+    echo $ipv6str >> $1
 fi
