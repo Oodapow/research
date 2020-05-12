2ce4f2efbae85aca9aeafc94ea5f48668bab3d4c
Oodapow <petre.v.alexandru@gmail.com>
2020-05-12 21:05:15 +0300

    fix psql

--- infrastructure/l1/postgresql/os/ubuntu/listen-to-all.sh
+++ infrastructure/l1/postgresql/os/ubuntu/listen-to-all.sh
@@ -17,7 +17,23 @@ if grep -q "^listen_addresses =.*" $1 ; then
     sed -i "/^#listen_addresses/a listen_addresses = '\*'" $1
 fi
 
-pg_hba=$(find / -name "pg_hba.conf" -print -quit)
-echo "host    all             all              0.0.0.0/0                       md5" >> pg_hba
-echo "host    all             all              ::/0                            md5" >> pg_hba
-printf "$pg_hba edited."
+ipv4str="host    all             all              0.0.0.0/0                       md5"
+ipv6str="host    all             all              ::/0                            md5"
+
+if grep -q ipv4str $2 ; then
+    echo "Correct IPv4 settings found, doing nothing"
+fi
+
+if ! grep -q ipv4str $2 ; then
+    echo "No IPv4 settings found, adding one at the end"
+    echo ipv4str >> $2
+fi
+
+if grep -q ipv6str $2 ; then
+    echo "Correct IPv6 settings found, doing nothing"
+fi
+
+if ! grep -q ipv6str $2 ; then
+    echo "No IPv6 settings found, adding one at the end"
+    echo ipv6str >> $2
+fi
--- infrastructure/l1/postgresql/os/ubuntu/main.sh
+++ infrastructure/l1/postgresql/os/ubuntu/main.sh
@@ -1,5 +1,5 @@
 curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1/postgresql/os/ubuntu/apt-install.sh | bash && \
-curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1/postgresql/os/ubuntu/listen-to-all.sh | bash -s -- $(find / -name "postgresql.conf" -print -quit) && \
+curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1/postgresql/os/ubuntu/listen-to-all.sh | bash -s -- $(find / -name "postgresql.conf" -print -quit) $(find / -name "pg_hba.conf" -print -quit) && \
 curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1/postgresql/os/ubuntu/restart-service.sh | bash && \
 curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1/postgresql/os/ubuntu/enable-service.sh | bash && \
 curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1/postgresql/os/ubuntu/init-db.sh | bash -s -- $1
