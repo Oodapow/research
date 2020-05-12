3f1f279f0c7903a53603943161a8e9f726344903
Oodapow <petre.v.alexandru@gmail.com>
2020-05-12 21:53:56 +0300

    fix psql

--- infrastructure/l1/postgresql/os/ubuntu/edit-pghba-conf.sh
+++ infrastructure/l1/postgresql/os/ubuntu/edit-pghba-conf.sh
@@ -1,5 +1,5 @@
-ipv4str="host    all             all              0.0.0.0/0                       md5"
-ipv6str="host    all             all              ::/0                            md5"
+ipv4str="host    all             all              0.0.0.0/0               md5"
+ipv6str="host    all             all              ::/0                    md5"
 
 if grep -q "$ipv4str" $1 ; then
     echo "Correct IPv4 settings found, doing nothing"
--- infrastructure/l1/postgresql/os/ubuntu/main.sh
+++ infrastructure/l1/postgresql/os/ubuntu/main.sh
@@ -3,5 +3,4 @@ curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastruct
 curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1/postgresql/os/ubuntu/edit-pghba-conf.sh | bash -s -- $(find / -name "pg_hba.conf" -print -quit) && \
 curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1/postgresql/os/ubuntu/restart-service.sh | bash && \
 curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1/postgresql/os/ubuntu/enable-service.sh | bash && \
-echo "Sleeping 5 seconds" && sleep 5 && \
 curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1/postgresql/os/ubuntu/init-db.sh | bash -s -- $1
