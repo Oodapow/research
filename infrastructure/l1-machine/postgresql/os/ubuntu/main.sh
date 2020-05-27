ea63d14ba802ba75a793a9df179e5416e04a6256
OodapowUiPath <44609907+OodapowUiPath@users.noreply.github.com>
2020-05-27 09:43:26 +0300

    psql conf fix
    search only in the `\etc` folder for the 2 conf files

--- infrastructure/l1-machine/postgresql/os/ubuntu/main.sh
+++ infrastructure/l1-machine/postgresql/os/ubuntu/main.sh
@@ -1,6 +1,6 @@
 curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1-machine/postgresql/os/ubuntu/apt-install.sh | bash && \
-curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1-machine/postgresql/os/ubuntu/edit-postgresql-conf.sh | bash -s -- $(find / -name "postgresql.conf" -print -quit)
-curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1-machine/postgresql/os/ubuntu/edit-pghba-conf.sh | bash -s -- $(find / -name "pg_hba.conf" -print -quit) && \
+curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1-machine/postgresql/os/ubuntu/edit-postgresql-conf.sh | bash -s -- $(find /etc -name "postgresql.conf" -print -quit)
+curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1-machine/postgresql/os/ubuntu/edit-pghba-conf.sh | bash -s -- $(find /etc -name "pg_hba.conf" -print -quit) && \
 curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1-machine/postgresql/os/ubuntu/restart-service.sh | bash && \
 curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1-machine/postgresql/os/ubuntu/enable-service.sh | bash && \
 curl -fsSL https://raw.githubusercontent.com/Oodapow/research/master/infrastructure/l1-machine/postgresql/os/ubuntu/init-db.sh | bash -s -- $1
